f87da51d8c09d60b0c710e3780d5a98b99d55028
Kwesi Bazil <kwesibazil@gmail.com>
2025-03-15 22:51:59 -0400

    feat: add keyboard navigation support for tabs and arrow keys
    styles: re-add padding clamp after removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,69 @@
-import Link from "@components/Link";
-import profileImg from '@assets/images/avatar-jessica.jpeg'
+import {useState} from 'react';
+import Link, {type Social} from "@components/Link";
+import profileImg from '@assets/images/avatar-jessica.jpeg';
 
-const links: string[] = ['github', 'frontend mentor', 'linkedIn', 'twitter', 'instagram']
+
+const links: Social[] = [
+  {name:'github', url: 'https://github.com/kwesibazil'},
+  {name:'frontend mentor', url: 'https://www.frontendmentor.io/profile/kwesibazil'},
+  {name:'linkedIn', url: 'https://www.linkedin.com/'},
+  {name:'twitter', url: 'https://x.com/'},
+  {name:'instagram', url: 'https://www.instagram.com/'},
+];
+
+
 
 export default function App():React.JSX.Element{
-  return(
-    <article className="card">
+  const [tab, setTab] = useState<number | null>(null)
+  const [disableHover, setDisableHover] = useState(false)
+
+
+  const handleKeyDown = (event:React.KeyboardEvent<HTMLUListElement>):void=>{
+    if(event.key === 'Tab' || event.key === 'ArrowUp' || event.key === 'ArrowDown')
+      setDisableHover(true);
+
+    if(event.key ==='ArrowUp')
+      setTab(currentTab => (currentTab === null || currentTab === 0) ? links.length - 1 : currentTab -1) ;
+
+    else if(event.key === 'ArrowDown')
+      setTab(currentTab => currentTab === null ? 0 : (currentTab + 1) % links.length);  
+  };
+
+
+  const handleMouseOver = (event:React.MouseEvent<HTMLUListElement>):void => {
+    if(event.target instanceof HTMLAnchorElement){
+      setTab(Number(event.target.getAttribute('data-index')));
+      setDisableHover(false);
+    }
+  };
+  
+
+
+
+  return( 
+    <article  className="card">
       <img className="card-img" src={profileImg} alt="profile" />
       <h1 className="card-title">Jessica Randall</h1>
       <p className="card-location">London, United Kingdom</p>
       <p className="card-text">"Front-end developer and avid reader"</p>
-      <ul className="card-link-group">
-        {links.map(link =>(
-          <Link key={link} social={link}/>
+      <ul 
+        role="tablist" 
+        onKeyDown={handleKeyDown} 
+        onMouseOver={handleMouseOver}
+        className={`card-link-group ${disableHover ? 'disable-hover' : ''}`}
+      >
+        {links.map((link, index) =>(
+          <Link
+            index={index}
+            social={link} 
+            key={link.name} 
+            current={tab === index}
+          />
         ))}
       </ul>
     </article>
   );
 };
+
+
+
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,7 +1,42 @@
-export default function Link({social}:{social:string}):React.JSX.Element{
+import {memo, useRef, useEffect} from 'react';
+
+
+export type Social = {
+  name: string;
+  url: string;
+};
+
+interface LinkProps{
+  index:number;
+  social: Social;
+  current: boolean;
+
+};
+
+function Link({social, current, index}:LinkProps):React.JSX.Element{
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  useEffect(()=>{
+    if (current && ref.current) 
+      ref.current.focus();
+  },[current]);
+
   return(
-    <li className="card-link">
-      {social}
+    <li className="card-item" role="presentation">
+      <a 
+        ref={ref}
+        role="tab" 
+        target="_blank"
+        href={social.url} 
+        data-index={index}
+        aria-selected={current}
+        rel="noopener noreferrer"
+        className={`card-link ${current ? 'focused' : ''}`}
+      >
+        {social.name}</a>
     </li>
   );
 };
+
+
+export default memo(Link);
